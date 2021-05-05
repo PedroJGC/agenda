55679778fc2b92cbd287082c6d7d1f91cc2300a6
PedroJGC <pedro.juliogc02@gmail.com>
2021-05-05 14:36:20 -0300

    Criado tabela de eventos para trabalhar com a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+# Fazer com que apareça a data da tabela junto com o titulo
+class EventoAdmin(admin.ModelAdmin):
+  list_display = ('titulo', 'data_evento', 'data_criação')
+  list_filter = ('titulo', 'data_evento',) # criar um filtro
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Criando os campos do Banco de Dados
+class Evento(models.Model):
+  titulo = models.CharField(max_length=100)
+  descricao = models.TextField(blank=True, null=True)
+  data_evento = models.DateTimeField(verbose_name='Data do Evento')
+  data_criação = models.DateTimeField(auto_now=True) # insere a hora do registro atualmente
+  usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'evento' # exigindo que o nome da tabela seja "evento"
+
+  # função que mostra o titulo da tabela
+  def __str__(self):
+      return self.titulo
